6f3b5ca4f35977fa8b94ad81e671590e02d689e4
Brian Becerra <elibecerrahdz@gmail.com>
2023-04-14 20:09:38 -0600

    styles

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,21 @@
 var header = document.querySelector('.header');
+var nav = document.querySelector('.nav');
 var nav_toggler = document.querySelector('.nav-toggler');
+var nav_shown = false;
 
-nav_toggler.addEventListener('click', function (event) {
-    header.classList.toggle('show');
+nav.addEventListener('transitionend', function () {
+    if (!nav_shown) {
+        nav.classList.add('d-none');
+    }
+});
+
+nav_toggler.addEventListener('click', function () {
+    if (!nav_shown) {
+        nav.classList.remove('d-none');
+        setTimeout(() => header.classList.add('show'), 0);
+    } else {
+        header.classList.remove('show');
+    }
+
+    nav_shown = !nav_shown;
 });
